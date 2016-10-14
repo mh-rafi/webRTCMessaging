1f20492f328f9bec512d804a9e23e2577145032c
Rafi <rafi.bogra@gmail.com>
2016-10-14 19:32:02 +0600

    ldj

--- public/message/message.js
+++ public/message/message.js
@@ -99,83 +99,91 @@ angular.module('newJobs.message', ['ngRoute', 'ngResource'])
 
 
 		// var avchatObj = null;
-        var conferenceId = "mtconfid3";
-        var appToken = "MDAxMDAxAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADYRzQeJwJ9kIT1W0phkyxfph65BBwLjVSKIQ0dFZTpODdrTSBwB97bp8GCTZyPmRD6CbT64Dh5R2kwRKM8DySov0OvOwMPtqOLD%2B7eobHmqsh3%2BPtpwU%2FDGlGAJOwpOYk%3D";
-        var sessionToken = getQSParam("t");
-        var participantId = getQSParam("pid");
-        if (!sessionToken) {
-            //login to get session token
-            //for example (get random id)
-            console.log('Not session');
-            participantId = Math.floor(Math.random() * 9999999999) + 1000000000;
+		var conferenceId = "mtconfid3";
+		var appToken = "MDAxMDAxAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADYRzQeJwJ9kIT1W0phkyxfph65BBwLjVSKIQ0dFZTpODdrTSBwB97bp8GCTZyPmRD6CbT64Dh5R2kwRKM8DySov0OvOwMPtqOLD%2B7eobHmqsh3%2BPtpwU%2FDGlGAJOwpOYk%3D";
+		var sessionToken = getQSParam("t");
+		var participantId = getQSParam("pid");
 
-            var redirectUrl = "url to send response with the session token"
-            redirectUrl = location.href + "?pid=" + participantId;
 
-            console.log(redirectUrl);
-                ooVooClient.authorization({
-                    token: appToken,
-                    isSandbox: true,
-                    userId: participantId,
-                    callbackUrl: redirectUrl
-                });
-        } else {
-            console.log(sessionToken);
-            ooVooClient.connect({
-                userId: participantId,
-                userToken: sessionToken
-            }, onClientConnected);
-        }
+		function onClientConnected(res) {
+			//init conference
+			avchatObj = ooVooClient.AVChat.init({
+				video: true,
+				audio: true,
+				videoResolution: ooVooClient.VideoResolution["HIGH"],
+				videoFrameRate: new Array(5, 15)
+			}, onAVChatInit);
+		}
 
-        function onClientConnected(res) {
-            //init conference
-            avchatObj = ooVooClient.AVChat.init({
-                video: true,
-                audio: true,
-                videoResolution: ooVooClient.VideoResolution["HIGH"],
-                videoFrameRate: new Array(5, 15)
-            }, onAVChatInit);
-        }
+		function onAVChatInit(res) {
+			if (!res.error) {
+				//register to conference events
+				avchatObj.onParticipantJoined = onParticipantJoined;
+				avchatObj.onParticipantLeft = onParticipantLeft;
+				avchatObj.onConferenceStateChanged = onConferenceStateChanged;
+				avchatObj.onRemoteVideoStateChanged = onRemoteVideoStateChanged;
+				avchatObj.join(conferenceId, participantId, "participant name", function(result) {});
+			}
+		}
 
-        function onAVChatInit(res) {
-            if (!res.error) {
-                //register to conference events
-                avchatObj.onParticipantJoined = onParticipantJoined;
-                avchatObj.onParticipantLeft = onParticipantLeft;
-                avchatObj.onConferenceStateChanged = onConferenceStateChanged;
-                avchatObj.onRemoteVideoStateChanged = onRemoteVideoStateChanged;
-                avchatObj.join(conferenceId, participantId, "participant name", function (result) { });
-            }
-        }
+		function onParticipantLeft(evt) {
+			if (evt.uid) {
+				document.getElementById("vid_" + evt.uid).remove();
+			}
+		}
 
-        function onParticipantLeft(evt) {
-            if (evt.uid) {
-                document.getElementById("vid_" + evt.uid).remove();
-            }
-        }
-        function onParticipantJoined(evt) {
-            if (evt.stream && evt.uid != null) {
-                if (evt.uid == participantId) { //me
-                    document.getElementById("localVideo").src = URL.createObjectURL(evt.stream);
-                }
-                else { //participants
-                    var videoElement = document.createElement("video");
-                    videoElement.id = "vid_" + evt.uid;
-                    videoElement.src = URL.createObjectURL(evt.stream);
-                    videoElement.setAttribute("autoplay", true);
-                    document.body.appendChild(videoElement);
-                }
-            }
-        }
-        function onConferenceStateChanged(evt) {
-        }
-        function onRemoteVideoStateChanged(evt) {
-        }
+		function onParticipantJoined(evt) {
+			if (evt.stream && evt.uid != null) {
+				if (evt.uid == participantId) { //me
+					document.getElementById("localVideo").src = URL.createObjectURL(evt.stream);
+				} else { //participants
+					var videoElement = document.createElement("video");
+					videoElement.id = "vid_" + evt.uid;
+					videoElement.src = URL.createObjectURL(evt.stream);
+					videoElement.setAttribute("autoplay", true);
+					document.body.appendChild(videoElement);
+				}
+			}
+		}
 
-        function getQSParam(name) {
-            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-                results = regex.exec(location.search);
-            return results === null ? "" : results[1].replace(/\+/g, " ");
-        }
+		function onConferenceStateChanged(evt) {}
+
+		function onRemoteVideoStateChanged(evt) {}
+
+		function getQSParam(name) {
+			name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+			var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+				results = regex.exec(location.search);
+			return results === null ? "" : results[1].replace(/\+/g, " ");
+		}
+
+
+		$scope.makeCall = function() {
+			if (!sessionToken) {
+				//login to get session token
+				//for example (get random id)
+				console.log('Not session');
+				participantId = Math.floor(Math.random() * 9999999999) + 1000000000;
+
+				var redirectUrl = "url to send response with the session token"
+				redirectUrl = location.href + "?pid=" + participantId;
+
+				console.log(redirectUrl);
+				ooVooClient.authorization({
+					token: appToken,
+					isSandbox: true,
+					userId: participantId,
+					callbackUrl: redirectUrl
+				});
+			} else {
+				console.log(sessionToken);
+				ooVooClient.connect({
+					userId: participantId,
+					userToken: sessionToken
+				}, onClientConnected);
+			}
+		};
+
+
+
 	})
